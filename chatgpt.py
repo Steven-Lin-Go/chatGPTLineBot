f50b8e5fdddfa819e9468dbf2fc1686d434cdf29
Steven Lin <lcc.goforit@gmail.com>
2022-12-30 14:39:15 +0800

    Update chatgpt.py key & model

--- chatgpt.py
+++ chatgpt.py
@@ -1,11 +1,11 @@
 from api.prompt import Prompt
 import os
 import openai
-openai.api_key = os.getenv("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")
+openai.api_key = "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"
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
-        self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
+        self.model = "chatbot"
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
         self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
         self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
